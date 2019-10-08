18596bc452eb22f28f8783023d254471f04fc86e
00 <1017861184@qq.com>
2019-10-08 12:31:23 +0800

    first commit

---
+++ testAppium.py
@@ -0,0 +1,17 @@
+from appium import webdriver
+from xml.dom.minidom import parse
+import xml.etree.ElementTree  as ET
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '9'
+desired_caps['deviceName'] = 'emulator-5554'
+desired_caps['appPackage'] = 'com.codbking.calendar.exaple'
+desired_caps['appActivity'] = 'com.codbking.calendar.exaple.MainActivity'
+
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+page_source = driver.page_source
+print(page_source)
+driver.find_element_by_id('com.codbking.calendar.exaple:id/text1').click()
+
+
