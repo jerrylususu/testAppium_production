9b94959e774c2d7a9e9028074485f41d98048d79
00 <1017861184@qq.com>
2020-03-04 18:17:23 +0800

    generate test case

--- generatetests/GenerateTests.py
+++ generatetests/GenerateTests.py
@@ -3,7 +3,7 @@ from appiumdriver.DriverConfig import config
 from processlogcat.ProcessLogFile import check_and_complete_test, generate_test
 
 desired_caps = config()
-appium_command = appium_driver(desired_caps, 'log/appiumlog.log', 'log/adblog.log', 'log/adberr.log', 10)
+appium_command = appium_driver(desired_caps, 'log/appiumlog.log', 'log/adblog.log', 'log/adberr.log', 30)
 print(appium_command)
-generate_test('log/adblog.log', appium_command, 'tests/tests.txt')
+generate_test('log/adblog.log', appium_command)
 
--- processlogcat/ProcessLogFile.py
+++ processlogcat/ProcessLogFile.py
@@ -8,12 +8,39 @@ keywords = {
                'send text': 'send keys to element command',
                'scroll': 'JSON Payload',
                'pinch/zoom': '/touch/multi/perform',
-               'keycode': 'Calling PressKeyCode',
-               'target API': 'TARGET API FOUND'
+               'keycode': 'Calling PressKeyCode'
     }
 
 driver_pinch = "width = driver.get_window_size()['width']"+"\n"+"height = driver.get_window_size()['height']"+"\n"+"action1 = TouchAction(driver).press(el=None, x=width * 0.2, y=height * 0.2).wait(1000).move_to(el=None,x=width * 0.4,y=height * 0.4).release()"+"\n"+"action2 = TouchAction(driver).press(el=None, x=width * 0.8, y=height * 0.8).wait(1000).move_to(el=None,x=width * 0.6,y=height * 0.6).release()"+"\n"+"pinch_action = MultiAction(driver)"+"\n"+"pinch_action.add(action1, action2)"+"\n"+"pinch_action.perform()"
 driver_zoom = "width = driver.get_window_size()['width']"+"\n"+"height = driver.get_window_size()['height']"+"\n"+"action1 = TouchAction(driver).press(el=None, x=width * 0.4, y=height * 0.4).wait(1000).move_to(el=None,x=width * 0.2,y=height * 0.2).release()"+"\n"+"action2 = TouchAction(driver).press(el=None, x=width * 0.6, y=height * 0.6).wait(1000).move_to(el=None,x=width * 0.8,y=height * 0.8).release()"+"\n"+"pinch_action = MultiAction(driver)"+"\n"+"pinch_action.add(action1, action2)"+"\n"+"pinch_action.perform()"
+
+
+def check_whether_trigger_native(file, out_file):
+    trigger_native_APIs = []
+    page_source = "jj"
+    with open(out_file, 'w', encoding='utf-8') as of:
+        with open(file, 'r', encoding='utf-8') as f:
+            line = f.readline().strip('\n')
+            while line:
+                if 'W System.err' in line:
+                    trigger_native_APIs.append(line)
+                    line = f.readline()
+                    while line and re.match(r"channel read: GET .*?/source", line) is None:
+                        if 'W System.err' in line:
+                            trigger_native_APIs.append(line)
+                        else:
+                            break
+                        line = f.readline().strip("\n")
+
+                    while "AppiumResponse" not in line:
+                        line = f.readline().strip("\n")
+                    page_source = line
+                    return trigger_native_APIs, page_source
+                else:
+                    of.write(line + "\n")
+                    line = f.readline().strip("\n")
+    return trigger_native_APIs, page_source
+
 
 def filter_adblogfile(file, out_file):
     with open(out_file, 'w') as of:
@@ -23,11 +50,8 @@ def filter_adblogfile(file, out_file):
                 line = f.readline().strip('\n')
                 for keyword in keywords.values():
                     if keyword in line:
-                        if keyword == 'TARGET API FOUND':
-                            return
-                        else:
-                            of.write(line+'\n')
-                            break
+                        of.write(line+'\n')
+                        break
 
 
 def check_line(resource_id, line, raw_command):
@@ -40,7 +64,7 @@ def check_line(resource_id, line, raw_command):
     elif keywords['send text'] in line:
         raw_command.append("driver.find_element_by_id({}).send_keys()".format(resource_id))
     elif keywords['scroll'] in line:
-        raw_command.append("driver.swipe()")
+        raw_command.append("driver.swipe")
     elif keywords['pinch/zoom'] in line:
         raw_command.append("multi_perform")
     elif keywords['keycode'] in line:
@@ -61,30 +85,43 @@ def transfer_log_to_raw_command(filter_adblog):
         return raw_command
 
 
-def check_and_complete_test(raw_command, appium_command, outfilepath):
+def check_and_complete_test(raw_command, appium_command, native_APIs):
     print(raw_command)
-    with open(outfilepath, "w") as of:
-        for i in range(len(raw_command)):
-                if raw_command[i] in appium_command[i]:
-                    if appium_command == 'driver.multi_perform_pinch()':
-                        of.write(driver_pinch+"\n")
-                    elif appium_command == 'driver.multi_perform_zoom()':
-                        of.write(driver_zoom+"\n")
+    if len(native_APIs) == 0:
+        with open('tests/test/test.txt', "w") as of:
+            for i in range(len(raw_command)):
+                    if raw_command[i] in appium_command[i]:
+                        if appium_command[i] == 'driver.multi_perform_pinch()':
+                            of.write(driver_pinch+"\n")
+                            print("okk")
+                        elif appium_command[i] == 'driver.multi_perform_zoom()':
+                            of.write(driver_zoom+"\n")
+                        else:
+                            of.write(appium_command[i]+"\n")
                     else:
-                        of.write(appium_command[i]+"\n")
-                else:
-                    of.write("error")
-                    break
+                        of.write("error")
+                        break
+    else:
+        with open('tests/ctest/test.txt', "w") as of:
+            for i in range(len(raw_command)):
+                    if raw_command[i] in appium_command[i]:
+                        if appium_command == 'driver.multi_perform_pinch()':
+                            of.write(driver_pinch+"\n")
+                        elif appium_command == 'driver.multi_perform_zoom()':
+                            of.write(driver_zoom+"\n")
+                        else:
+                            of.write(appium_command[i]+"\n")
+                    else:
+                        of.write("error")
+                        break
 
 
-def generate_test(adblog, appium_command, outfile):
-    # TODO
-    filter_adblogfile(adblog, 'log/filter_adblog.log')
+def generate_test(adblog, appium_command):
+    native_APIs, page_source = check_whether_trigger_native('log/adblog.log', 'log/test.log')
+    filter_adblogfile('log/test.log', 'log/filter_adblog.log')
     raw_command = transfer_log_to_raw_command('log/filter_adblog.log')
-    check_and_complete_test(raw_command, appium_command, outfile)
+    check_and_complete_test(raw_command, appium_command, native_APIs)
 
 
 if __name__ == '__main__':
-    # TODO
-    appium_command = []
-    generate_test('log/adblog.log', appium_command, 'test/testcase.txt')
+    native, page = check_whether_trigger_native('log/adblog.log', 'log/out.log')
