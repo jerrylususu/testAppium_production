cf55f9b3a5182164bf29bb172cf88f5eee506d54
00 <1017861184@qq.com>
2020-04-03 23:31:41 +0800

    on 10.20.48.184

--- GenerateTests.py
+++ GenerateTests.py
@@ -1,17 +1,39 @@
 from time import sleep
-
+import traceback
+import sys
+from appiumdriver_processlog.appiumdriver.util.ProcessText import delete_text
 from appiumdriver_processlog.appiumdriver.AppiumDriver import appium_driver
 from appiumdriver_processlog.processlog.ProcessLogFile import generate_test
 
+# jerrylu scripts
+import docker
+from initialize_utils.docker_init import docker_init
+from initialize_utils.adb_connect_install import adb_connect_install
+
+# docker related config
+image_name="budtmo/docker-android-x86-8.1"
+gui_port=6080
+appium_port=4723
+emu_port=5564
+adb_port=5565
+
+# adb related config
+adb_exe_path="/home/jerrylu/adb/platform-tools/adb"
+adb_connection_str="localhost:" + str(adb_port)  # this is generated at runtime
+apk_path="/home/jerrylu/mineapk/de.danoeh.antennapod.apk"
+
+# appium desired caps
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
-desired_caps['platformVersion'] = '9'
-desired_caps['deviceName'] = 'emulator-5554'
+desired_caps['platformVersion'] = '8.1'
+desired_caps['deviceName'] = 'emulator-5554' # This should be fine... They are all called `emulator-5554` internally inside container
 desired_caps['appPackage'] = 'de.danoeh.antennapod'
 desired_caps['appActivity'] = 'de.danoeh.antennapod.activity.SplashActivity'
 desired_caps['eventTimings'] = True
 desired_caps['automationName'] = 'UIAutomator2'
-i = 0
+
+# test related data containers
+test_num = 0
 # record target APIs triggered
 trigger_native_APIs = set()
 # record activities triggered
@@ -21,16 +43,59 @@ widgets = set()
 # record widgets in page_source
 widgets_page_source = set()
 
-while i < 3:
+# clear the last log
+try:
+    delete_text("log/appium.log")
+except Exception as e:
+    print(e)
+    exc_type, exc_value, exc_traceback_obj = sys.exc_info()
+    traceback.print_tb(exc_traceback_obj)
+
+
+# initialize
+client = docker.from_env()
+container = docker_init(client=client, image_name=image_name, gui_port=gui_port, appium_port=appium_port, emu_port=emu_port, adb_port=adb_port)
+adb_connect_install(adb_exe_path=adb_exe_path, adb_connection_str=adb_connection_str,apk_path=apk_path)
+remote_addr = "http://localhost:"+str(appium_port)+"/wd/hub"
+
+# main test loop
+while test_num < 3:
     try:
-        print("\n{} test:\n".format(i))
-        appium_command = appium_driver(desired_caps, 50, activities, widgets, widgets_page_source)
+        print("\n{} test:\n".format(test_num))
+        appium_command = appium_driver(desired_caps, 10, activities, widgets, widgets_page_source, test_num, remote_addr=remote_addr, adb_exe_path=adb_exe_path)
+        # appium_command = appium_driver(desired_caps, 10, activities, widgets, widgets_page_source, test_num)
+        print("\n"+"appium_command:")
         print(appium_command)
-        generate_test(appium_command, i, trigger_native_APIs)
-    except Exception:
-        print("error: {}".format(i) + "test")
-    i += 1
+        generate_test(appium_command, test_num, trigger_native_APIs)
+    except Exception as e:
+        print(e)
+        print("error: {}".format(test_num) + "test")
+        exc_type, exc_value, exc_traceback_obj = sys.exc_info()
+        traceback.print_tb(exc_traceback_obj)
+    test_num += 1
     sleep(5)
+
+# tear down: remove container
+container.remove(force=True)
+print("{*} container removed.")
+
+# output & save results
+print("triggered activities:")
 print(activities)
+print("triggered executable elements:")
 print(widgets)
+print("all achievable executable elements")
 print(widgets_page_source)
+print("widget coverage:")
+coverage = len(widgets)/len(widgets_page_source)
+print(str(coverage))
+
+with open('report/report.txt', "w") as f:
+    f.write("triggered activities:"+"\n")
+    f.write(str(activities)+"\n")
+    f.write("triggered executable elements:"+"\n")
+    f.write(str(widgets)+"\n")
+    f.write("all achievable executable elements"+"\n")
+    f.write(str(widgets_page_source)+"\n")
+    f.write("widget coverage:"+"\n")
+    f.write(str(coverage))
