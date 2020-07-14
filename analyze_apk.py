81083ee34f1d131593055f7a0cc622f9b8d59902
Jerry Lu <jerrylu.mail@qq.com>
2020-07-14 14:24:33 +0800

    fix ui

--- analyze_apk.py
+++ analyze_apk.py
@@ -8,13 +8,14 @@ if __name__ == "__main__":
     lines = []
     with open(apk_file, "r") as f:
         lines = f.readlines()
-    
+
     apk_file_root = Path(apk_file).parent
     with open(apk_file_root / "apk_version_output.txt", "w") as f2:
         for line in lines:
-            print(line)
+            line = line.strip()
             apk = base_apk_root / f"{line}.apk"
+            print(apk)
             SDKversion, package, main_activity, minSdk = analyse_apk(apk)
             print(f"SDKversion, package, main_activity, minSdk: {(SDKversion, package, main_activity, minSdk)}")
-            f2.write(SDKversion)
+            f2.write(str(SDKversion))
             f2.write("\n")
