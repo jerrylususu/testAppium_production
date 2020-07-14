1da5b56ecfde45031f7b28b1468e86c144f97718
Jerry Lu <jerrylu.mail@qq.com>
2020-07-14 12:59:17 +0800

    add use.sh

---
+++ mq_replay/use.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+
+
+# params
+PYTHON_PATH=/usr/bin/python3
+SCRIPT_PATH=docker_replay_mq_pool_runner.py
+INPUT_FILE=pool_status
+OUTPUT_FILE=pool_pid
+
+BASH_REDIRECT_FOLDER=syslog
+PYTHON_LOG_FOLDER=pylog
+
+mkdir $BASH_REDIRECT_FOLDER
+mkdir $PYTHON_LOG_FOLDER
+
+# empty the ouput file
+> $OUTPUT_FILE
+
+echo "start!"
+
+while read line; do
+    $PYTHON_PATH $SCRIPT_PATH $line >$BASH_REDIRECT_FOLDER/$line.out 2>$BASH_REDIRECT_FOLDER/$line.err &
+    pid=$!
+    echo "$pid|$line" >> $OUTPUT_FILE 
+
+    echo "started on $line, pid is $pid"
+done < $INPUT_FILE
+
+echo "all started!"
